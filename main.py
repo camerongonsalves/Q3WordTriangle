156da79b0fee510d02894727bc86cb4910e1dc9f
Cameron <113189878+camerongonsalves@users.noreply.github.com>
2022-12-08 11:30:19 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+userinput=input("Please enter your word!")
+negative=-1
+ran=""
+print()
+for i in range(0,len(userinput)):
+  negative=negative+1
+  ran=ran+userinput[negative]
+  print(ran)
+  task=0
+print()
+print("Have a nice day!")
